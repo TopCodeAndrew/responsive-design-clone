0ccbebc52baafabb41dfa87387d76f012f591e78
Andrew Smith <topcodeandrew@gmail.com>
2020-12-05 16:24:21 -0700

    multiple responsive points done

--- responsive-design-clone/src/App.js
+++ responsive-design-clone/src/App.js
@@ -1,28 +1,55 @@
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  function toggleMenu() {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
     <div>
       <header className='header'>
-        <h1 className='bootstrap'> Start Bootstrap</h1>
-        <button className='header-button'>
-          <p>MENU</p>
-          <img className='hamburger' src='https://icon-library.com/images/white-menu-icon/white-menu-icon-4.jpg' />
-        </button>
+        <div className='container'>
+          <h1 className='bootstrap'> Start Bootstrap</h1>
+          <div className='larger-nav'>
+            <p>SERVICES</p>
+            <p>PORTFOLIO</p>
+            <p>ABOUT</p>
+            <p>TEAM</p>
+            <p>CONTACT</p>
+          </div>
+          <button className='header-button' onClick={toggleMenu}>
+            <div>MENU</div>
+            <img className='hamburger' src='https://icon-library.com/images/white-menu-icon/white-menu-icon-4.jpg' alt='top background' />
+          </button>
+        </div>
 
-        <nav className='mobile-nav'></nav>
+        <nav className={`mobile-nav ${isMenuOpen ? null : 'hide-mobile-nav'}`}>
+          <div className='mobile-nav-container'>
+            <p>SERVICES</p>
+            <p>PORTFOLIO</p>
+            <p>ABOUT</p>
+            <p>TEAM</p>
+            <p>CONTACT</p>
+          </div>
+        </nav>
 
       </header>
 
       <div className='top-main'>
         <h2 className='welcome'> Welcome To Our Studio </h2>
         <div className="meet">IT'S NICE TO MEET YOU</div>
-        <div className='more-info'>TELL ME MORE</div>
+        <div className='more-info'>
+          <p className='tell'>TELL ME MORE</p>
+        </div>
 
 
       </div>
-    </div>
+    </div >
   );
 }
 
-export default App;
+
